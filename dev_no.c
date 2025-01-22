483e1866aaad38a93ff65f9fe4ad7e6275a65498
Celina Sophie Kalus <hello@celinakalus.de>
2025-01-22 21:51:40 +0100

    dev_no: Create device file from within the module
    Signed-off-by: Celina Sophie Kalus <hello@celinakalus.de>

--- dev_no.c
+++ dev_no.c
@@ -1,4 +1,8 @@
 /* SPDX-License-Identifier: GPL-3.0 */
+#include <linux/dev_printk.h>
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/kdev_t.h>
 #include <linux/export.h>
 #include <linux/fs.h>
 #include <linux/printk.h>
@@ -9,6 +13,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/device/class.h>
 
 MODULE_DESCRIPTION("/dev/no device driver module");
 MODULE_AUTHOR("Celina Kalus <hello@celinakalus.de>");
@@ -20,6 +25,7 @@ static int no_open(struct inode *, struct file *);
 static int no_show(struct seq_file *, void *);
 
 static int major;
+static dev_t dev_no_devt;
 
 static const char *no_list[] = {
 		"Not in this lifetime.",
@@ -52,6 +58,21 @@ static const struct file_operations no_fops = {
 	.release = single_release,
 };
 
+/* attribute for device class of /dev/no */
+static struct attribute *class_dev_no_attrs[] = {
+	NULL,
+};
+ATTRIBUTE_GROUPS(class_dev_no);
+
+/* /dev/no device class */
+static struct class class_dev_no = {
+	.name = "dev_no",
+	.class_groups = class_dev_no_groups,
+};
+
+/* pointer to the kobj corresponding to /dev/no */
+struct device *dev_no_device;
+
 static int no_open(struct inode *inode, struct file *file)
 {
 	return single_open(file, no_show, NULL);
@@ -71,6 +92,7 @@ static __init int my_init(void)
 {
 	int retval = 0;
 
+	/* register the character device */
 	retval = register_chrdev(0, NO_CHRDEV_NAME, &no_fops);
 
 	if (retval <= 0) {
@@ -80,8 +102,30 @@ static __init int my_init(void)
 	}
 
 	major = retval;
+	dev_no_devt = MKDEV(major, 0);
 
-	pr_info("Registered char device with major %i.\n", major);
+	/*
+	 * create a device class. if this fails, it's okay to return early
+	 * without unregistering the char device; the user will just have to
+	 * create the device file /dev/no themselves.
+	 */
+	retval = class_register(&class_dev_no);
+
+	if (retval < 0) {
+		pr_err("Could not register device class: %i\n", retval);
+
+		return 0;
+	}
+
+	dev_no_device = device_create(&class_dev_no, NULL, dev_no_devt, NULL, "no");
+
+	if (PTR_ERR_OR_ZERO(dev_no_device)) {
+		pr_err("Unable to create device /dev/no: %li\n", PTR_ERR(dev_no_device));
+
+		return 0;
+	}
+
+	dev_info(dev_no_device, "Registered device /dev/no with major %i.\n", major);
 
 	return 0;
 }
@@ -89,6 +133,8 @@ module_init(my_init);
 
 static __exit void my_exit(void)
 {
+	device_destroy(&class_dev_no, dev_no_devt);
+	class_unregister(&class_dev_no);
 	unregister_chrdev(major, NO_CHRDEV_NAME);
 }
 module_exit(my_exit);
